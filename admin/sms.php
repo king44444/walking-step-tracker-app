6249237a24f9d719d68f442899f1573f350acb7f
King <mking44444@gmail.com>
2025-10-10 21:16:33 -0600

    Create admin/sms.php bridge file to route requests to AdminSmsController

---
+++ admin/sms.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+\App\Core\Env::bootstrap(dirname(__DIR__));
+require_once __DIR__ . '/../api/lib/admin_auth.php';
+require_admin();
+if (session_status() !== PHP_SESSION_ACTIVE) session_start();
+
+use App\Controllers\AdminSmsController;
+
+$controller = new AdminSmsController();
+$method = $_SERVER['REQUEST_METHOD'];
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// Route to controller methods
+if ($method === 'GET') {
+    // /admin/sms -> index
+    if (preg_match('#^/admin/sms$#', $path)) {
+        echo $controller->index();
+    }
+    // /admin/sms/messages -> messages
+    elseif (preg_match('#^/admin/sms/messages$#', $path)) {
+        echo $controller->messages();
+    }
+    else {
+        http_response_code(404);
+        echo json_encode(['error' => 'Not found']);
+    }
+}
+elseif ($method === 'POST') {
+    // /admin/sms/send -> send
+    if (preg_match('#^/admin/sms/send$#', $path)) {
+        echo $controller->send();
+    }
+    // /admin/sms/upload -> upload
+    elseif (preg_match('#^/admin/sms/upload$#', $path)) {
+        echo $controller->upload();
+    }
+    // /admin/sms/start-user -> startUser
+    elseif (preg_match('#^/admin/sms/start-user$#', $path)) {
+        echo $controller->startUser();
+    }
+    else {
+        http_response_code(404);
+        echo json_encode(['error' => 'Not found']);
+    }
+}
+else {
+    http_response_code(405);
+    echo json_encode(['error' => 'Method not allowed']);
+}
